563ba3937c4157ea622e438b628591781f5cff2d
abida hassan <jeanarnolde1995@gmail.com>
2020-02-21 09:55:15 +0100

    move secteur & local impl to the impl package

--- taxe-boisson-v2/src/main/java/com/example/taxBoisson/service/TypeRedevableService.java
+++ taxe-boisson-v2/src/main/java/com/example/taxBoisson/service/TypeRedevableService.java
@@ -7,9 +7,5 @@ import com.example.taxBoisson.bean.TypeRedevable;
 public interface TypeRedevableService {
 public TypeRedevable findByLibelle(String libelle);
 public void save(TypeRedevable typeRedevable);
-public List<TypeRedevable> findAll();
-	
-
-	
-
+public List<TypeRedevable> findAll();	
 }
